d9ac61e1f44fb5f85929fe53d36749f1600efe3d
negamann303 <negamann303@e06d52c1-3ea7-368a-827f-cea7ba5c3d3d>
2014-10-11 22:57:53 +0000

    Reverted to r8...

--- compiled/zip.sh
+++ compiled/zip.sh
@@ -2,7 +2,7 @@
 
 export INITRAMFS=`readlink -f .`
 export INSTALLER=$INITRAMFS/installer
-REVISION="$(svn info https://kernel-nae-negalite.googlecode.com/svn/ | grep "^Revision:" | cut -c 11-)"
+REVISION="$(svn info https://kernel-ng2-negalite.googlecode.com/svn/ | grep "^Revision:" | cut -c 11-)"
 
 perl repack-bootimg.pl zImage boot.img-ramdisk boot.img 
 mv boot.img ./installer/kernel/boot.img
@@ -10,6 +10,6 @@ mv boot.img ./installer/kernel/boot.img
 echo "Zipping The Kernel Up For Flashable Package"
 cd $INSTALLER
 
-zip -9 -r negalite_kernel_NAE cpu kernel META-INF setup system
-mv $INSTALLER/negalite_kernel_NAE.zip $INITRAMFS/negalite_kernel_NAE_r$REVISION.zip
+zip -9 -r negalite_kernel_NG2 cpu kernel META-INF setup system
+mv $INSTALLER/negalite_kernel_NG2.zip $INITRAMFS/negalite_kernel_NG2_r$REVISION.zip
 echo "Done"
