775cbd24a1b56fc2816fa9996d0a556a0fa82e4a
negamann303 <negamann303@e06d52c1-3ea7-368a-827f-cea7ba5c3d3d>
2014-10-12 07:16:51 +0000

    Added Smartass and Badass Governors Updated Config Fixed Ramdisk

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -423,6 +423,12 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
+extern struct cpufreq_governor cpufreq_gov_badass;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_badasss)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
+extern struct cpufreq_governor cpufreq_gov_smartass;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass)
 #endif
 
 
