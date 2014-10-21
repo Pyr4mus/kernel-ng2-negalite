58c57e6830096825ee8f2238851af330739a9aba
negamann303 <negamann303@e06d52c1-3ea7-368a-827f-cea7ba5c3d3d>
2014-10-21 02:11:28 +0000

    OverClocking: Achieved(45 Steps Total/Max: 2.21GHz)

--- include/linux/acpuclock-defines.h
+++ include/linux/acpuclock-defines.h
@@ -1,6 +1,6 @@
 #define FREQ_TABLE_SIZE		45
 #define FREQ_TABLE_SIZE_OFFSET	8
-#define FREQ_STEPS		37
+#define FREQ_STEPS		45
 #define MAX_VDD_SC		1450000 /* uV */
 #define MIN_VDD_SC		600000 /* uV */
 #define MAX_VDD_MEM_DIG		1150000 /* uV */
